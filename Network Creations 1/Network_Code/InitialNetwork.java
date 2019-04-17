5c4cd616ff5032e12c7da34bbe532ca71a195021
Noah del Angel <noahdelangel@gmail.com>
2019-04-17 13:24:31 -0500

    -Discovered location of NaN error prior to epoch

--- Network Creations 1/Network_Code/InitialNetwork.java
+++ Network Creations 1/Network_Code/InitialNetwork.java
@@ -152,7 +152,7 @@ public class InitialNetwork implements DNA
     {
         String fileContent = finaloutput;
      
-        BufferedWriter writer = new BufferedWriter(new FileWriter("outputfile.dat"));
+        BufferedWriter writer = new BufferedWriter(new FileWriter("/Users/noahdelangel/Desktop/School/Portfolio Project/MusicNeuralNetwork/Network Creations 1/Network_Code/outputfile.dat"));
         writer.write(fileContent);
         writer.close();
     }
