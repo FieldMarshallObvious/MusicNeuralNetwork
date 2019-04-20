c14dd647b8fb9c86b5f4efece46d7e402009a820
FieldMarshallObvious <noahdelangel@gmail.com>
2019-04-20 10:51:45 -0500

    test

--- Network Creations 1/Network_Code/Brain.java
+++ Network Creations 1/Network_Code/Brain.java
@@ -4,48 +4,40 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+public class Brain {
+	// Note: need to write handeling logic for Exception
+	public static void main(String[] args) throws IOException {
 
-public class Brain
-{
-	//Note: need to write handeling logic for Exception  
-	public static void main(String[] args) throws IOException
-	{
-		
-		String inputfile = "/Users/noahdelangel/Desktop/School/Portfolio Project/MusicNeuralNetwork/Network Creations 1/Network_Code/inputfile.dat";
-		String outputfile = "/Users/noahdelangel/Desktop/School/Portfolio Project/MusicNeuralNetwork/Network Creations 1/Network_Code/outputfile.dat";
-		String expectedfile = "/Users/noahdelangel/Desktop/School/Portfolio Project/MusicNeuralNetwork/Network Creations 1/Network_Code/excpected.dat";
+		String inputfile = "inputfile.dat";
+		String outputfile = "outputfile.dat";
+		String expectedfile = "excpected.dat";
 
-		double learningRate = 0.05;
+		double learningRate = 0.1;
 		double Momentum = 0.25;
 
-		int epochs = 10000;
+		int epochs = 100000;
 		int dataSize = dataSize(inputfile);
 
-		InitialNetwork initNet = new InitialNetwork(dataSize, dataSize-1, learningRate);
+		InitialNetwork initNet = new InitialNetwork(dataSize, dataSize - 1, learningRate);
 		ForgettingNetwork forgettingNet = new ForgettingNetwork(dataSize, dataSize - 1, learningRate);
 		SelectionNetwork selectNet = new SelectionNetwork(dataSize, dataSize - 1, learningRate);
-		
 
-
-		for(int e = 0; e <= epochs; e++)
-		{
+		for (int e = 0; e <= epochs; e++) {
 			System.out.println("Entering Input Network");
 			initNet.settingInputs((setInputs(inputfile, 89)));
 			initNet.writeOutputs();
-			
+
 			System.out.println("Entering Forgetting Network");
 			forgettingNet.settingInputs((setInputs(outputfile, 89)));
 			forgettingNet.writeOutputs();
-			//clearing old outputs
-			//pwOut.close();
-
+			// clearing old outputs
+			// pwOut.close();
 
 			System.out.println("Entering Selection Network");
 			selectNet.settingInputs((setInputs(outputfile, 89)));
-			//clearing old outputs
-			//pwOut.close();
+			// clearing old outputs
+			// pwOut.close();
 			selectNet.writeOutputs();
-
 
 			System.out.print("Epoch: " + e);
 
@@ -66,38 +58,33 @@ public class Brain
 			System.out.println();
 		}
 
-		//Once reachers final itteration implement piano use
-
+		// Once reachers final itteration implement piano use
 
 	}
 
-	private static double[] getExpectedOutput(String fileLocation, int dataSize) throws FileNotFoundException
-	{
+	private static double[] getExpectedOutput(String fileLocation, int dataSize) throws FileNotFoundException {
 		int x = 0;
 
 		double[] output = new double[dataSize];
 
 		Scanner expectedDataScanner = new Scanner(new File(fileLocation));
-		
-		while(expectedDataScanner.hasNext())
-		{
+
+		while (expectedDataScanner.hasNext()) {
 			output[x] = Double.valueOf(expectedDataScanner.nextLine());
 			x++;
 		}
 
 		return output;
 	}
-	
-	//sets inputs of specific data size and at a specific location in the array
-	private static double[] setInputs(String filelocation, int dataSize) throws FileNotFoundException
-	{
+
+	// sets inputs of specific data size and at a specific location in the array
+	private static double[] setInputs(String filelocation, int dataSize) throws FileNotFoundException {
 		int x = 0;
 		double[] output = new double[dataSize];
 
 		Scanner inputDataScanner = new Scanner(new File(filelocation));
 
-		while(inputDataScanner.hasNext())
-		{
+		while (inputDataScanner.hasNext()) {
 			output[x] = Double.valueOf(inputDataScanner.nextLine());
 			x++;
 		}
@@ -105,26 +92,21 @@ public class Brain
 
 		return output;
 	}
-	
-	//gives size of data, makes cheeky NASA reference
-	private static int dataSize(String fileLocation) throws FileNotFoundException
-	{
+
+	// gives size of data, makes cheeky NASA reference
+	private static int dataSize(String fileLocation) throws FileNotFoundException {
 		int output = 0;
 		Scanner inputDataReadScanner = new Scanner(new File(fileLocation));
-		try
-		{
-			//System.out.println("First line in data " + inputDataReadScanner.nextLine());
-			while(inputDataReadScanner.hasNext())
-			{
+		try {
+			// System.out.println("First line in data " + inputDataReadScanner.nextLine());
+			while (inputDataReadScanner.hasNext()) {
 				inputDataReadScanner.nextLine();
-				output+=1;
+				output += 1;
 			}
 			inputDataReadScanner.close();
-		
+
 			return output;
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println("Houston we have a problem!");
 			System.out.println("We have a failure due to " + e.getClass().getSimpleName());
 			return 0;
